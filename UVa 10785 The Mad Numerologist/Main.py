e8f1f0bf656f7dea001a7b4ab1226d041e7fa264
ndvssankar <sivashankar@msitprogram.net>
2020-03-17 00:25:09 +0530

    The mad numeralogist

--- UVa 10785 The Mad Numerologist/Main.py
+++ UVa 10785 The Mad Numerologist/Main.py
@@ -1,12 +1,42 @@
-t = int(input())
-d = {}
-s = "AJS,BKT,CLU,DMV,ENW,FOX,GPY,HOZ,IR"
-freq = {}
-l = s.split(",")
-for i in range(0, len(l)):
-    d[i + 1] = l[i]
-print(d)
-for i in range(t):
+v = "AUEOI"
+vowels = []
+for vowel in v:
+    vowels += [vowel] * 21
+
+c = "JSBKTCLDMVNWFXGPYHQZR"
+consonants = []
+
+for consonant in c:
+    consonants += [consonant] * 5
+
+n_tests = int(input())
+for t in range(n_tests):
+    vlst = []
+    clst = []
     n = int(input())
     
-        
+    vlst = vowels[0:n//2]
+    clst = consonants[0:n//2]
+
+    if n % 2:
+        vlst.append(vowels[n//2])
+    vlst.sort()
+    clst.sort()
+    
+    flag = False
+    c = 0
+    v = 0
+    res = []
+    # res = "".join(a+b for a,b in zip(vlst, clst))
+    for i in range(n):
+        if flag == False and v < len(vlst):
+            res.append(vlst[v])
+            flag = True
+            v += 1
+        else:
+            if c < len(clst):
+                res.append(clst[c])
+                flag = False
+                c += 1
+    res = "Case " + str(t+1) + ": " + "".join(res)
+    print (res)
