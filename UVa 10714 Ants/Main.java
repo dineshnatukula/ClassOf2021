4853f8ad8560c5d06716b960260699557e52f1b3
ndvssankar <sivashankar@msitprogram.net>
2020-03-17 19:48:49 +0530

    Added ants files and others

--- UVa 10714 Ants/Main.java
+++ UVa 10714 Ants/Main.java
@@ -6,31 +6,19 @@ public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int t = scan.nextInt();
-        for (int i = 0; i < t; i++) {
+        while (t-- > 0) {
             int l = scan.nextInt();
             int n = scan.nextInt();
-            int half = l/2;
-            int m1 = 0, m2 = 0;
-            for (int j = 0; j < n; j++) {
-                int distAnt = scan.nextInt();
-                if (distAnt < half) {
-                    if (m1 < distAnt) {
-                        m1 = distAnt;
-                    }
-                    if (m2 < (l - distAnt)) {
-                        m2 = (l - distAnt);
-                    }
-                } else {
-                    if (m1 < (l - distAnt)) {
-                        m1 = (l - distAnt);
-                    }
-                    if (m2 < distAnt) {
-                        m2 = distAnt;
-                    }
-                }
+            int min = 0, max = 0;
+            while(n-- > 0) {
+                int x = scan.nextInt();
+                x = x < l-x ? x : l-x;
+                if (x > min)
+                    min = x;
+                if (l-x > max)
+                    max = l - x;
             }
-            System.out.println(m1 + " " + m2);
+            System.out.println(min + " " + max);
         }
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
     }
 }
